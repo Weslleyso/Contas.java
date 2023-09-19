471bfb3e62300dec94e33eda3acf6f7b0ffde687
Weslley <106591789+Weslleyso@users.noreply.github.com>
2023-09-18 22:26:57 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,22 +1,24 @@
-public class main {
+public class main{
     public static void main (String[] args){
         ContaBanco p1 = new ContaBanco();
-        p1.setNumconta(111);
+
+        p1.setNumConta(111);
         p1.setDono("Marcos");
         p1.abrirconta("CC");
-        
+
         ContaBanco p2 = new ContaBanco();
-        p2.setNumconta(222);
+
+        p2.setNumConta(222);
         p2.setDono("Larissa");
         p2.abrirconta("CP");
 
         p1.depositar(100);
         p2.depositar(500);
         p2.sacar(100);
-        
+
         p1.estadoAtual();
         p2.estadoAtual();
 
+
     }
-    
 }
